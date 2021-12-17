2eb4c2c4ea694eb546fc84ec30031d447f9d0585
David Bronicki <dbronick@go.olemiss.edu>
2021-12-17 02:05:37 -0600

    pre-break commit

--- source/chess_module/chess-rules.test.js
+++ source/chess_module/chess-rules.test.js
@@ -1,67 +1,95 @@
 const chessRules = require("./chess-rules")
 
-test("Index move construction", () => {
-	expect(new chessRules.Move(4, 8)).toEqual({
-		startSquare: 4,
-		endSquare: 8,
-		promotionRule: "Q"
-	})
-})
-test("Index move construction with promo", () => {
-	expect(new chessRules.Move(4, 8, 'B')).toEqual({
-		startSquare: 4,
-		endSquare: 8,
-		promotionRule: "B"
-	})
-})
-test("Algebraic move construction", () => {
-	expect(new chessRules.Move("e1", "a2")).toEqual({
-		startSquare: 4,
-		endSquare: 8,
-		promotionRule: "Q"
-	})
+test("Move type tests", () => {
+	expect(moveTest()).toEqual(moveResults())
 })
 
-let startposString = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-let startposObject = {
-	blackToPlay: false,
-	blackLong: true,
-	blackShort: true,
-	whiteLong: true,
-	whiteShort: true,
-	enPassantSquare: 64,
-	moveNumber: 1,
-	plySinceCaptureOrPawnMove: 0,
-	pieces: [
-		'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R',
-		'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
-		'_', '_', '_', '_', '_', '_', '_', '_',
-		'_', '_', '_', '_', '_', '_', '_', '_',
-		'_', '_', '_', '_', '_', '_', '_', '_',
-		'_', '_', '_', '_', '_', '_', '_', '_',
-		'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
-		'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
-	]
-}
-
-test("Test fen startpos", () => {
-	expect(new chessRules.Board(startposString)).toEqual(startposObject)
-})
-
-test("Test copy startpos", () => {
-	expect(new chessRules.Board(undefined, 
-		new chessRules.Board(startposString))).toEqual(startposObject)
-})
-
-test("Test startpos to fen", () => {
-	expect(new chessRules.Board(startposString).toFen()).toEqual(startposString)
+test("Board functions test", () => {
+	expect(boardTest()).toEqual(boardResults())
 })
 
 // test("Game object building and move generation", () => {
 // 	expect((new chessRules.Board(startposString)).generateLegalMoves()).toEqual(1)
 // })
 
-function fenGen(){
+test("Game stack tests", () => {
+	expect(stackTest()).toEqual(stackResults())
+})
+
+// test("Performance test of position 1 with depth = 4", () => {
+// 	expect(perft2("startpos", 4)).toEqual(197281)
+// })
+test("Performance test of position 1 with depth = 3", () => {
+	expect(perft2("startpos", 3)).toEqual(8902)
+	// expect(perft2("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1", 1)).toEqual(400)
+})
+
+// test("Performance test of position 2 with depth = 4", () => {
+// 	expect(perft2("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4)).toEqual(4085603)
+// })
+// test("Performance test of position 2 with depth = 2", () => {
+// 	expect(perft2("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2)).toEqual(2039)
+// })
+
+// test("Performance test of position 3 with depth = 4", () => {
+// 	expect(perft2("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 4)).toEqual(43238)
+// })
+
+// test("Performance test of position 4 with depth = 4", () => {
+// 	expect(perft2("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 4)).toEqual(422333)
+// })
+
+function moveTest(){
+	return [
+		new chessRules.Move(4, 8),
+		new chessRules.Move(4, 8, 'B'),
+		new chessRules.Move("e1", "a2")
+	]
+}
+
+function moveResults(){
+	return [
+		{
+			startSquare: 4,
+			endSquare: 8,
+			promotionRule: "Q"
+		},
+		{
+			startSquare: 4,
+			endSquare: 8,
+			promotionRule: "B"
+		},
+		{
+			startSquare: 4,
+			endSquare: 8,
+			promotionRule: "Q"
+		}
+	]
+}
+
+function boardTest(){
+	let board = new chessRules.Board('startpos')
+	let fen1 = board.toFen()
+	board = new chessRules.Board(board)
+	let fen2 = board.toFen()
+	let board = new chessRules.Board("rnbqkbnr/pppp1ppp/4p3/8/8/4P3/PPPP1PPP/RNBQKBNR w KQkq - 0 2")
+	return [
+		new chessRules.Board('startpos'),
+		new chessRules.Board(undefined, 
+			new chessRules.Board(startposString)),
+		new chessRules.Board(startposString).toFen()
+	]
+}
+
+function boardResults(){
+	return [
+		startposObject,
+		startposObject,
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+	]
+}
+
+function stackTest(){
 	let game = new chessRules.ChessGame('startpos')
 
 	game.performMove(new chessRules.Move('b1', 'c3'))
@@ -88,16 +116,12 @@ function fenGen(){
 	}
 }
 
-function expectedFens(){
+function stackResults(){
 	return {
 		firstFen: "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1",
 		secondFen: "rnbqkbnr/ppp1pppp/8/3N4/8/8/PPPPPPPP/R1BQKBNR b KQkq - 0 2"
 	}
 }
-
-test("Game stack handling", () => {
-	expect(fenGen()).toEqual(expectedFens())
-})
 
 function perft2(fen, depth){
 	// let fen = "startpos"
@@ -118,25 +142,23 @@ function perft2(fen, depth){
 	return helperF(depth)
 }
 
-// test("Performance test of position 1 with depth = 4", () => {
-// 	expect(perft2("startpos", 4)).toEqual(197281)
-// })
-test("Performance test of position 1 with depth = 3", () => {
-	expect(perft2("startpos", 3)).toEqual(8902)
-	// expect(perft2("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR b KQkq - 0 1", 1)).toEqual(400)
-})
-
-// test("Performance test of position 2 with depth = 4", () => {
-// 	expect(perft2("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4)).toEqual(4085603)
-// })
-// test("Performance test of position 2 with depth = 2", () => {
-// 	expect(perft2("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 2)).toEqual(2039)
-// })
-
-// test("Performance test of position 3 with depth = 4", () => {
-// 	expect(perft2("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 4)).toEqual(43238)
-// })
-
-// test("Performance test of position 4 with depth = 4", () => {
-// 	expect(perft2("r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1", 4)).toEqual(422333)
-// })
+var startposObject = {
+	blackToPlay: false,
+	blackLong: true,
+	blackShort: true,
+	whiteLong: true,
+	whiteShort: true,
+	enPassantSquare: 64,
+	moveNumber: 1,
+	plySinceCaptureOrPawnMove: 0,
+	pieces: [
+		'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R',
+		'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P',
+		'_', '_', '_', '_', '_', '_', '_', '_',
+		'_', '_', '_', '_', '_', '_', '_', '_',
+		'_', '_', '_', '_', '_', '_', '_', '_',
+		'_', '_', '_', '_', '_', '_', '_', '_',
+		'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p',
+		'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r',
+	]
+}
